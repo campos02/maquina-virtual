e4b96aca22a11e73a80e09f203f0aa004c1e3ca8
campos <nonesovile96@hotmail.com>
2025-11-03 19:44:47 -0300

    Adicionar botão de carregar programa

--- src/janela.rs
+++ src/janela.rs
@@ -1,37 +1,61 @@
+use crate::maquina::maquina::Maquina;
 use eframe::egui;
+use rfd::FileDialog;
 
-#[derive(Default)]
 pub struct Janela {
-    //botao_1: u32,
+    maquina: Maquina,
+}
+
+impl Default for Janela {
+    fn default() -> Self {
+        Self {
+            maquina: Maquina::new(),
+        }
+    }
 }
 
 impl eframe::App for Janela {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // Área principal (fora da sidebar). Cuidado com o espaço horizontal para a sidebar não comer a area
-            ui.heading("Área principal");
-            ui.separator();
-            ui.horizontal(|ui| {
-                ui.add_space(200.0);
-                ui.label("janela pra tralhas.");
-            })
-        });
-
         // barra lateral
         egui::SidePanel::left("sidebar")
             .resizable(false)
             .default_width(200.0)
             .show(ctx, |ui| {
-                ui.heading("");
+                ui.add_space(6.0);
+                ui.heading("Registradores");
                 ui.separator();
-
-                // espaçamento
-                ui.add_space(20.0);
-
-                // The button
-                if ui.button("Traduções").clicked() {
-                    //implementar tabela de traduções
-                }
             });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            // Área principal (fora da sidebar). Cuidado com o espaço horizontal para a sidebar não comer a área
+            ui.heading("Área principal");
+            ui.separator();
+
+            if ui.button("Carregar programa (hex)").clicked() {
+                let arquivo = FileDialog::new()
+                    .set_title("Carregar um programa")
+                    .add_filter("Arquivo Hex SIC/XE (.hex)", &["hex"])
+                    .pick_file();
+
+                if let Some(hex) = arquivo
+                    && let Ok(hex) = std::fs::read_to_string(hex)
+                {
+                    let hex = hex.trim();
+                    let mut chars = hex.chars();
+                    let mut bytes = Vec::with_capacity(hex.len() / 2);
+
+                    // Converter pares de dígitos hexa para bytes
+                    while let (Some(digito1), Some(digito2)) = (chars.next(), chars.next()) {
+                        let byte = [digito1, digito2].iter().collect::<String>();
+                        if let Ok(byte) = u8::from_str_radix(&byte, 16) {
+                            bytes.push(byte);
+                        }
+                    }
+
+                    // TODO: Mensagem de erro caso o carregamento falhe
+                    let _ = self.maquina.carregar(&bytes);
+                }
+            }
+        });
     }
 }
